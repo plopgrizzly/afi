6a4e41c916cc4330af67a765cba7426d8533bf89
Jeron Aldaron Lau <jeron.lau@plopgrizzly.com>
2017-10-08 15:24:29 -0500

    Added some code, icon and issue template.

---
+++ ISSUE_TEMPLATE.md
@@ -0,0 +1,23 @@
+# Bug Report
+
+## Target platform
+(Windows 10 / Fedora / MacOS / Android 4.4.4 / etc)
+
+## Description of what went wrong
+
+## Code sample (if applicable)
+
+## Screenshot (if applicable)
+
+
+
+# Feature Request
+
+## Description of feature
+
+
+
+# Support Request
+
+## Target platform and/or backed
+(XCB on Linux)
---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// Aldaron's Format Interface
+// Copyright (c) 2017 Plop Grizzly, Jeron Lau <jeron.lau@plopgrizzly.com>
+// Licensed under the MIT LICENSE
+
+//! Aldaron's Format Interface is a library developed by Plop Grizzly for
+//! reading and writing different formats.
+
+#![no_std]
+#![warn(missing_docs)]
+#![doc(html_logo_url = "http://plopgrizzly.com/afi/icon.png",
+	html_favicon_url = "http://plopgrizzly.com/afi/icon.png",
+	html_root_url = "http://plopgrizzly.com/afi/")]
+
+extern crate ami;
+
+use ami::Vec;
+
+/// A graphic.  Format: `(W, H, [RGBA Pixels])`
+pub struct Graphic(pub Vec<u32>);
+
+/// Sound.  Format: `(Hz, [Samples])`
+pub struct Sound(pub Vec<u16>);
+
+/// Text.  Format `([Characters])`
+pub struct Text(pub Vec<u8>);
